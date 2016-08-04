bd967dc291f6e96808d32db58475d589cc9474da
Moore.Huang <moore.hy@gmail.com>
2016-08-04 21:44:11 +0800

    Feature: 实现json字串解析工具方法
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spiderZhiHu.py
+++ spiderZhiHu.py
@@ -21,5 +21,9 @@ print news.text
 
 class Utils(object):
     @staticmethod
+    def json_loads(raw):
+        return json.loads(raw, encoding="UTF-8")
+
+    @staticmethod
     def json_dumps(raw):
         return json.dumps(raw, indent=None, encoding="UTF-8", ensure_ascii=False)
