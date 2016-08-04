8446967c0eabd223bde1e3443fb852a5da1cb274
Moore.Huang <moore.hy@gmail.com>
2016-08-04 21:40:44 +0800

    Feature: 制定TODO列表，工作目标
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spiderZhiHu.py
+++ spiderZhiHu.py
@@ -5,6 +5,11 @@
 import requests
 # import time
 
+# TODO 1) 实现抓取当天文章, 并存入DB, DB应包含是否已阅读, 是否感兴趣, 是否TOP, 以及相关TAG(此数据阅读后更新)
+# TODO 2) 从DB中取出当天的文章, 生成对应的Bookmarks文件(将[TOP], [D]标志表现在文章title中), 用于导入到浏览器
+# TODO 3) 将DB中的文章请求并保存到本地
+# TODO 4) 将本地保存的文章生成为mobi文件, 并发送到Kindle
+
 headers = {'Host': 'news-at.zhihu.com',
            'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) '
                          'AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'}
