adf1fb1d6c213f0699933ebc7746d026d75c4f8f
Moore.Huang <moore.hy@gmail.com>
2016-08-05 16:08:40 +0800

    Feature: 调用工具，将新闻转换为dict对象
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spiderZhiHu.py
+++ spiderZhiHu.py
@@ -6,6 +6,8 @@ import sys
 import requests
 # import time
 
+from model.Utils import Utils
+
 # TODO 1) 实现抓取当天文章, 并存入DB, DB应包含是否已阅读, 是否感兴趣, 是否TOP, 以及相关TAG(此数据阅读后更新)
 # TODO 2) 从DB中取出当天的文章, 生成对应的Bookmarks文件(将[TOP], [D]标志表现在文章title中), 用于导入到浏览器
 # TODO 3) 将DB中的文章请求并保存到本地
@@ -20,3 +22,5 @@ news = requests.get('http://news-at.zhihu.com/api/4/news/latest', headers=header
 if news.status_code != 200:
     print(u'获取最新文章列表失败: [%s]%s' % (news.status_code, news.text))
     sys.exit(1)
+
+news_content = Utils.json_loads(news.text)
