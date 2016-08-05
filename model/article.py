32661dda7c81a23893dd9bb19ca461a440d08c1b
Moore.Huang <moore.hy@gmail.com>
2016-08-05 16:50:09 +0800

    Feature: 实现通过时间和字典数据初始化文章类的方法
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- model/article.py
+++ model/article.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 # Author: Moore.Huang <moore@moorehy.com>
 
+import sys
+
+from Utils import Utils
+
 
 class Article(object):
     def __init__(self):
@@ -13,3 +17,14 @@ class Article(object):
         self.star = 0
         self.top = 0
         self.tags = u''  # 若为多个tag, 则以','分隔
+
+    def init_with_time_and_data(self, timestamp, data):
+        self.timestamp = timestamp
+        try:
+            self.article_id = data['id']
+            self.title = data['title']
+            self.images = ','.join(data['images'])
+        except Exception as e:
+            Utils.print_log(u'初始化解析data字典异常: %s' % e, prefix=u'[Err]')
+            sys.exit(1)
+        return self
