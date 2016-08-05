00f18e52464d82c5833171488e2f7f61866dcdf3
Moore.Huang <moore.hy@gmail.com>
2016-08-05 16:29:05 +0800

    Feature: 设置默认编码为utf8
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spiderZhiHu.py
+++ spiderZhiHu.py
@@ -7,6 +7,9 @@ import requests
 
 from model.Utils import Utils
 
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 # TODO 1) 实现抓取当天文章, 并存入DB, DB应包含是否已阅读, 是否感兴趣, 是否TOP, 以及相关TAG(此数据阅读后更新)
 # TODO 2) 从DB中取出当天的文章, 生成对应的Bookmarks文件(将[TOP], [D]标志表现在文章title中), 用于导入到浏览器
 # TODO 3) 将DB中的文章请求并保存到本地
