1b39a072907817ea27fd909dc9417b0b2175885f
Moore.Huang <moore.hy@gmail.com>
2016-08-13 22:13:58 +0800

    Feature: 更新TODO列表
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spiderZhiHu.py
+++ spiderZhiHu.py
@@ -11,9 +11,9 @@ from model.Zhihu import Zhihu
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-# TODO 1) 实现抓取当天文章, 并存入DB, DB应包含是否已阅读, 是否感兴趣, 是否TOP, 以及相关TAG(此数据阅读后更新)
+# DONE 1) 实现抓取当天文章, 并存入DB, DB应包含是否已阅读, 是否感兴趣, 是否TOP, 以及相关TAG(此数据阅读后更新)
 # TODO 2) 从DB中取出当天的文章, 生成对应的Bookmarks文件(将[TOP], [D]标志表现在文章title中), 用于导入到浏览器
-# TODO 3) 将DB中的文章请求并保存到本地
+# DONE 3) 将DB中的文章请求并保存到本地
 # TODO 4) 将本地保存的文章生成为mobi文件, 并发送到Kindle
 
 
