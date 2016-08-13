8c6b73351909b9f3b853073be746407a83234793
Moore.Huang <moore.hy@gmail.com>
2016-08-13 21:49:42 +0800

    Feature: 实现命令行参数解析执行逻辑
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spiderZhiHu.py
+++ spiderZhiHu.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 # Author: Moore.Huang <moore@moorehy.com>
 
+import argparse
 import sys
 
-from model.Utils import Utils
+import config
 from model.Zhihu import Zhihu
 
 reload(sys)
@@ -17,5 +18,36 @@ sys.setdefaultencoding('utf8')
 
 
 zhihu = Zhihu()
-zhihu.spider_articles_from_net()
-zhihu.print_articles()
+
+cmd_list = ['spider', 'local']
+parser = argparse.ArgumentParser()
+parser.add_argument("command", choices=cmd_list, help=u"要执行的子命令")
+args = parser.parse_args(sys.argv[1:2])
+
+args_child_list = sys.argv[2:]
+if args.command == 'spider':
+    des = u'''例子:
+    ./spdierZhiHu.py spider  # 抓取当日的文章列表, 插入到DB
+    ./spdierZhiHu.py spider -s  # 抓取当日的文章列表, 插入到DB, 并将文章保存到本地
+    ./spdierZhiHu.py spider -n -s  # 从DB获取当日的文章列表, 并将文章保存到本地
+    ./spdierZhiHu.py spider -d 20160809  # 抓取20160809的文章列表, 插入到DB
+    ./spdierZhiHu.py spider -d 20160809 -s  # 抓取20160809的文章列表, 插入到DB, 并将文章保存到本地
+    ./spdierZhiHu.py spider -d 20160809 -n -s  # 从DB获取20160809的文章列表, 并将文章保存到本地
+    '''
+    parser_info = argparse.ArgumentParser(prog=u'spiderZhiHu.py %s' % args.command,
+                                          formatter_class=argparse.RawDescriptionHelpFormatter,
+                                          description=des)
+    parser_info.add_argument('-d', '--date', default=None, help=u"指定日期")
+    parser_info.add_argument('-s', '--save', action='store_true', help=u"保存文章到本地")
+    parser_info.add_argument('-n', '--no_spider', action='store_true', help=u"不抓取文章列表, 从DB获取")
+    args_info = parser_info.parse_args(args_child_list)
+
+    if args_info.no_spider:
+        zhihu.load_articles_from_db(args_info.date)
+    else:
+        zhihu.spider_articles_from_net(args_info.date)
+
+    if args_info.save:
+        zhihu.spider_articles_html(config.article_archive_abs_path)
+    else:
+        zhihu.print_articles()
