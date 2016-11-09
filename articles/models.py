61d0a063082d3c93d9750870449ddc759d8f7e69
Moore.Huang <moore.hy@gmail.com>
2016-11-09 21:34:59 +0800

    Feature: 实现文章标签数据模型
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.html import format_html
+
+
+class Tag(models.Model):
+    theme = models.CharField(verbose_name='主题',
+                             max_length=50,
+                             unique=True)
+
+    def __str__(self):
+        return self.theme
+
+    class Meta:
+        verbose_name = '文章主题'
+        verbose_name_plural = verbose_name
