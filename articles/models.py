36e713cc1cfc4c4ef88b013071c0a93539637680
Moore.Huang <moore.hy@gmail.com>
2016-11-09 21:36:43 +0800

    Feature: 实现文章数据模型，用来存储爬取到的文章基础信息
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- articles/models.py
+++ articles/models.py
@@ -16,3 +16,21 @@ class Tag(models.Model):
     class Meta:
         verbose_name = '文章主题'
         verbose_name_plural = verbose_name
+
+
+class Article(models.Model):
+    pub_datetime = models.DateTimeField(verbose_name='发布时间')
+    title = models.CharField(verbose_name='标题',
+                             max_length=255)
+    local_url = models.CharField(verbose_name='本地URL',
+                                 unique=True,
+                                 max_length=255)
+
+    tags = models.ManyToManyField(Tag,
+                                  blank=True,
+                                  verbose_name='标签')
+
+    class Meta:
+        verbose_name = '文章基本'
+        verbose_name_plural = verbose_name
+        ordering = ('-pub_datetime',)
