2af739be17c9af877ad1992ff6f17d98156e3363
Moore.Huang <moore.hy@gmail.com>
2016-11-09 21:38:43 +0800

    Feature: 实现阅读记录模型，记录每位读者的阅读文章以及对文章的打分&读后感
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- articles/models.py
+++ articles/models.py
@@ -55,3 +55,24 @@ class ZhihuDaily(models.Model):
     class Meta:
         verbose_name = '知乎日报'
         verbose_name_plural = verbose_name
+
+
+STAR_CHOICES = (
+    (0, '-'),
+    (1, '★'),
+    (2, '★★'),
+    (3, '★★★'),
+    (4, '★★★★'),
+    (5, '★★★★★'),
+)
+
+
+class ReadRecord(models.Model):
+    reader = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='读者')
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL, verbose_name='文章')
+    star = models.SmallIntegerField(verbose_name='文章评级', default=0, choices=STAR_CHOICES)
+    comment = models.TextField(verbose_name='读后感', null=True, default=None)
+
+    class Meta:
+        verbose_name = '阅读记录'
+        verbose_name_plural = verbose_name
