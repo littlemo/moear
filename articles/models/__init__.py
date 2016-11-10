3a7888582c61b442e44c27b9abbb400252033452
Moore.Huang <moore.hy@gmail.com>
2016-11-10 11:57:07 +0800

    Optimize: 剥离文章相关模型到模型包中的articles文件中
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- articles/models/__init__.py
+++ articles/models/__init__.py
@@ -1,3 +1,7 @@
 # 导入基础组件模型
 from .components import Tag
 from .components import Source
+
+# 导入文章模型
+from .articles import Article
+from .articles import ZhihuDaily
---
+++ articles/models/articles.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+from .components import Source
+from .components import Tag
+
+
+class Article(models.Model):
+    pub_datetime = models.DateTimeField(verbose_name='发布时间')
+    title = models.CharField(verbose_name='标题', max_length=255)
+    source = models.ForeignKey(Source, verbose_name='来源')
+    url = models.CharField(verbose_name='原文URL', unique=True, max_length=255)
+    local_url = models.CharField(verbose_name='本地URL', unique=True, null=True, default=None, max_length=255)
+
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='标签')
+
+    class Meta:
+        verbose_name = '文章基本'
+        verbose_name_plural = verbose_name
+        ordering = ('-pub_datetime',)
+
+
+class ZhihuDaily(models.Model):
+    article = models.OneToOneField(Article, on_delete=models.CASCADE, verbose_name='文章')
+    daily_id = models.IntegerField(verbose_name='日报ID')
+    cover_images = models.CharField(verbose_name='封面图片', null=True, default=None, max_length=255)
+    top = models.BooleanField(verbose_name='热文')
+
+    def __str__(self):
+        return self.article
+
+    def fmt_tags_list(self):
+        return '，'.join([str(tag) for tag in Tag.objects.filter(article__pk=self.pk)])
+
+    class Meta:
+        verbose_name = '知乎日报'
+        verbose_name_plural = verbose_name
