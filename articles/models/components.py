d863c9c0203cb966db66a44f3dfa21bc94aaf7fb
Moore.Huang <moore.hy@gmail.com>
2016-11-11 22:07:53 +0800

    Optimize: 优化文章源模型属性，增加一个文章本地化开关属性

--- articles/models/components.py
+++ articles/models/components.py
@@ -23,6 +23,7 @@ class Source(models.Model):
     文章源组件，用来指示文章来源的名称，以及未来的爬取规则&方法
     """
     name = models.CharField(verbose_name='来源名称', max_length=50, unique=True)
+    localization = models.BooleanField(verbose_name='文章本地化')
     # TODO V2.1.0 增加爬取时间规则时，在此处增加相应的 `rules` 属性
 
     def __str__(self):
