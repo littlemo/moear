e2d8141420c61a33838fc1450b1f708340ed47ec
Moore.Huang <moore.hy@gmail.com>
2016-11-21 23:19:26 +0800

    Feature: 实现知乎日报爬虫的的开始连接处理，获取到所有需要爬取的文章信息
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spider/spider/spiders/zhihu_daily.py
+++ spider/spider/spiders/zhihu_daily.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import json
+
 import scrapy
 
 
@@ -6,8 +8,24 @@ class ZhihuDailySpider(scrapy.Spider):
     name = "zhihu_daily"
     allowed_domains = ["zhihu.com"]
     start_urls = (
-        'http://www.zhihu.com/',
+        'http://news-at.zhihu.com/api/4/news/latest',
     )
 
     def parse(self, response):
-        pass
+        return self.yield_article_request(response=response)
+
+    def yield_article_request(self, response):
+        content_raw = response.body.decode()
+        self.logger.debug('响应body原始数据：{}'.format(content_raw))
+        content = json.loads(content_raw, encoding='UTF-8')
+        self.logger.debug(content)
+
+        self.logger.info('日期：{}'.format(content['date']))
+
+        self.logger.info('今日文章')
+        for item in content['stories']:
+            self.logger.info(item)
+
+        self.logger.info('头条文章')
+        for item in content['top_stories']:
+            self.logger.info(item)
