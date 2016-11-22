6312b58de34d7daebb6ca9269143b0e6a3b51900
Moore.Huang <moore.hy@gmail.com>
2016-11-22 12:26:20 +0800

    Optimize: 调整并完善文章item的属性定义&说明信息
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spider/spider/items.py
+++ spider/spider/items.py
@@ -9,15 +9,15 @@ import scrapy
 
 
 class ArticleItem(scrapy.Item):
-    pub_datetime = scrapy.Field()  # 文章发布的日期时间，格式为：yyyy-mm-dd HH:MM:SS
+    pub_datetime = scrapy.Field()  # 文章发布的日期时间，需使用time包格式化为struct_time对象
     title = scrapy.Field()  # 文章标题
-    source = scrapy.Field()  # 文章来源，如：知乎日报
+    source = scrapy.Field()  # 文章来源，使用Django的Source模型填充
     url = scrapy.Field()  # 文章URL
-    local_url = scrapy.Field()  # 文章持久化后的本地路径
+    content = scrapy.Field()  # 文章正文
 
-    addition_info = scrapy.Item()  # 一些具体文章源的附加信息，如知乎日报的封面图、是否热文、文章ID
+    addition_info = scrapy.Field()  # 一些具体文章源的附加信息，如知乎日报的封面图、是否热文、文章ID
 
     # 以下参数为pipelines处理时使用
-    content = scrapy.Field()  # 文章正文
+    local_url = scrapy.Field()  # 文章持久化后的本地路径
     image_urls = scrapy.Field()  # 图片链接
     images = scrapy.Field()  # 图片存储返回
