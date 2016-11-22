a0f3ed6553004c5b1111bdba9830e8083800fc05
Moore.Huang <moore.hy@gmail.com>
2016-11-22 10:53:24 +0800

    Feature: 修改文章源模型，增加显示用名称以及描述信息
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- articles/models/components.py
+++ articles/models/components.py
@@ -22,7 +22,10 @@ class Source(models.Model):
     """
     文章源组件，用来指示文章来源的名称，以及未来的爬取规则&方法
     """
-    name = models.CharField(verbose_name='来源名称', max_length=50, unique=True)
+    name = models.CharField(verbose_name='来源名称', max_length=50, unique=True)  # 此处为spider对应的name，唯一
+    verbose_name = models.CharField(verbose_name='显示名称', max_length=100)  # 用于界面显示
+    description = models.CharField(verbose_name='描述信息', max_length=255, default=None)  # 描述信息
+
     persistent = models.BooleanField(verbose_name='文章持久化', default=False)
     # TODO V2.1.0 增加爬取时间规则时，在此处增加相应的 `rules` 属性
 
