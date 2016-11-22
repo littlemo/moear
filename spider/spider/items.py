46c1f7bfd499165773beb3e9749ee359944d11a3
Moore.Huang <moore.hy@gmail.com>
2016-11-22 12:27:44 +0800

    Feature: 实现文章源item定义，并添加save_to_db方法
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spider/spider/items.py
+++ spider/spider/items.py
@@ -7,6 +7,9 @@
 
 import scrapy
 
+from articles.models import Source
+from django.db import models
+
 
 class ArticleItem(scrapy.Item):
     pub_datetime = scrapy.Field()  # 文章发布的日期时间，需使用time包格式化为struct_time对象
@@ -21,3 +24,24 @@ class ArticleItem(scrapy.Item):
     local_url = scrapy.Field()  # 文章持久化后的本地路径
     image_urls = scrapy.Field()  # 图片链接
     images = scrapy.Field()  # 图片存储返回
+
+
+class SourceItem(scrapy.Item):
+    """
+    此模型默认使用spider信息填充，可手动修改
+    """
+    name = scrapy.Field()  # 来源名称，唯一，长度<50
+    verbose_name = scrapy.Field()  # 显示名称，长度<100
+    description = scrapy.Field()  # 描述信息，长度<255
+
+    def save_to_db(self):
+        try:
+            src = Source.objects.get(name=self.get('name'))
+            src.verbose_name = self.get('verbose_name')
+            src.description = self.get('description', None)
+            src.save()
+        except models.ObjectDoesNotExist:
+            src = Source.object.create(name=self.get('name'),
+                                       verbose_name=self.get('verbose_name'),
+                                       description=self.get('description'))
+        return src
