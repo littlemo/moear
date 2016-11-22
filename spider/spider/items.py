827492004f1f444ce32c6e62f068f7cbcb5e7fdb
Moore.Huang <moore.hy@gmail.com>
2016-11-22 10:15:46 +0800

    Feature: 创建Spider工程的item文章模型
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spider/spider/items.py
+++ spider/spider/items.py
@@ -8,7 +8,16 @@
 import scrapy
 
 
-class SpiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ArticleItem(scrapy.Item):
+    pub_datetime = scrapy.Field()  # 文章发布的日期时间，格式为：yyyy-mm-dd HH:MM:SS
+    title = scrapy.Field()  # 文章标题
+    source = scrapy.Field()  # 文章来源，如：知乎日报
+    url = scrapy.Field()  # 文章URL
+    local_url = scrapy.Field()  # 文章持久化后的本地路径
+
+    addition_info = scrapy.Item()  # 一些具体文章源的附加信息，如知乎日报的封面图、是否热文、文章ID
+
+    # 以下参数为pipelines处理时使用
+    content = scrapy.Field()  # 文章正文
+    image_urls = scrapy.Field()  # 图片链接
+    images = scrapy.Field()  # 图片存储返回
