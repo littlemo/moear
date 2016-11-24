57c2459134ea4c7a2029e641caa5c28e689d6c26
Moore.Huang <moore.hy@gmail.com>
2016-11-24 21:01:51 +0800

    Feature: 设置静态文件URL&本地路径，并加入文章页所需静态图片
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- moear/settings.py
+++ moear/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+else:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
