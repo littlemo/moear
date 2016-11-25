29e704609407ffd3638fb02f375bfe2b1bb8df3b
Moore.Huang <moore.hy@gmail.com>
2016-11-25 21:04:10 +0800

    Feature: 完善Spider批处理调用脚本，增加命令行参数输入以及日期转换异常处理
    Signed-off-by: Moore.Huang <moore.hy@gmail.com>

--- spider/auto.py
+++ spider/auto.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import datetime
 import os
 import sys
@@ -7,9 +8,26 @@ import sys
 次脚本用来批量执行爬取操作，指定开始&结束日期
 """
 
+des = u'''例子:
+    ./auto.py 20161029 20161101  # 抓取[20161029, 20161101)的文章
+    '''
+parser = argparse.ArgumentParser(prog=u'auto.py',
+                                 formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description=des)
+parser.add_argument("begin", help=u"开始日期(包含)，格式yyyymmdd")
+parser.add_argument("end", help=u"结束日期(不含)，格式yyyymmdd")
+
+args = parser.parse_args()
+begin = args.begin
+end = args.end
+
 formater = '%Y%m%d'
-date_begin = datetime.datetime.strptime('20161101', formater)
-date_end = datetime.datetime.strptime('20161111', formater)
+try:
+    date_begin = datetime.datetime.strptime(begin, formater)
+    date_end = datetime.datetime.strptime(end, formater)
+except ValueError as e:
+    print('日期格式错误(yyyymmdd): {}'.format(e))
+    sys.exit(1)
 date_current = date_begin
 print('开始日期：{}\n结束日期：{}'.format(date_begin.strftime(formater), date_end.strftime(formater)))
 
@@ -17,7 +35,7 @@ if date_begin > date_end:
     print('Error: 起始日期晚于结束日期！')
     sys.exit(1)
 
-while date_current <= date_end:
+while date_current < date_end:
     result = os.popen('scrapy crawl zhihu_daily -a date={} -a force=True'.format(date_current.strftime(formater)))
     for line in result:
         print(line)
