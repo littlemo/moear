5eb96907e905108d3d5bee85ec339a08edb3d42f
Moore.Huang <moore@moorehy.com>
2018-02-04 14:33:57 +0800

    O: 修改auto.py调用爬虫的自动化脚本，将原位置参数修改为键值参数

--- spider/auto.py
+++ spider/auto.py
@@ -9,17 +9,31 @@ import sys
 """
 
 des = u'''例子:
-    ./auto.py 20161029 20161101  # 抓取[20161029, 20161101)的文章
+    ./auto.py -b 20161029 -e 20161101  # 抓取[20161029, 20161101)的文章
     '''
 parser = argparse.ArgumentParser(prog=u'auto.py',
                                  formatter_class=argparse.RawDescriptionHelpFormatter,
                                  description=des)
-parser.add_argument("begin", help=u"开始日期(包含)，格式yyyymmdd")
-parser.add_argument("end", help=u"结束日期(不含)，格式yyyymmdd")
+parser.add_argument("-b", "--begin", help=u"开始日期(包含)，格式yyyymmdd", default=None)
+parser.add_argument("-e", "--end", help=u"结束日期(不含)，格式yyyymmdd", default=None)
 
 args = parser.parse_args()
+
+log_file = ' --logfile {}.log'.format(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 'log', datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')))
+print('| log file path: {}'.format(log_file))
+
 begin = args.begin
 end = args.end
+
+if begin == end == None:
+    result = os.popen('scrapy crawl zhihu_daily{}'.format(log_file))
+    sys.exit(0)
+
+if begin == None or end == None:
+    print('日期参数错误: {} | {}'.format(begin, end))
+    sys.exit(1)
 
 formater = '%Y%m%d'
 try:
@@ -34,10 +48,6 @@ print('开始日期：{}\n结束日期：{}'.format(date_begin.strftime(formater
 if date_begin > date_end:
     print('Error: 起始日期晚于结束日期！')
     sys.exit(1)
-
-log_file = ' --logfile {}.log'.format(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                 'log', datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')))
 
 while date_current < date_end:
     result = os.popen('scrapy crawl zhihu_daily -a date={} -a force=True{}'
