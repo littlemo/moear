d73e17cc563f42c1cafd951aa61a2feee7a2f2d1
Moore.Huang <moore@moorehy.com>
2018-02-17 14:09:25 +0800

    M: 修改项目包相关的定义，由moear修改为server

--- server/server/settings.py
+++ server/server/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'moear.urls'
+ROOT_URLCONF = 'server.urls'
 
 TEMPLATES = [
     {
@@ -77,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'moear.wsgi.application'
+WSGI_APPLICATION = 'server.wsgi.application'
 
 
 # Database
