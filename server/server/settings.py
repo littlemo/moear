376c4a33fdf61710707596f34fce5ee2a199076b
Moore.Huang <moore@moorehy.com>
2018-02-17 14:06:28 +0800

    M: 修改静态文件路径设置，将其输出到运行时目录下

--- server/server/settings.py
+++ server/server/settings.py
@@ -134,15 +134,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(RUNTIME_DIR, 'static')
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
-else:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
 
 # Logging Settings
 # https://docs.djangoproject.com/en/2.0/topics/logging/
