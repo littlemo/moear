c64cab4dbd68527d175a124cf33fd9ec5a11976c
Moore.Huang <moore@moorehy.com>
2018-02-18 11:00:35 +0800

    F: 实现用户元数据模型，用于存储指定用户的额外配置信息
    便于后期插件&爬虫扩展支持

--- server/core/models.py
+++ server/core/models.py
@@ -38,3 +38,41 @@ class Option(models.Model):
     class Meta:
         verbose_name = _('站点配置')
         verbose_name_plural = verbose_name
+
+
+class UserMeta(models.Model):
+    """
+    用户元数据模型
+
+    用于存储指定用户的元数据信息，便于对该用户进行参数定制修改，可与插件/爬虫配合使用
+    """
+    id = models.BigAutoField(
+        primary_key=True)
+    user = models.ForeignKey(
+        'auth.User',
+        verbose_name=_('用户信息'),
+        db_index=True,
+        on_delete=models.CASCADE)
+    key = models.CharField(
+        verbose_name=_('键名'),
+        db_index=True,
+        blank=True,
+        null=True,
+        default=None,
+        max_length=255)
+    value = models.TextField(
+        verbose_name=_('键值'),
+        blank=True,
+        null=True,
+        default=None)
+
+    def __str__(self):
+        output = '[{id}]{key} => {value}'.format(
+            id=self.id,
+            key=self.key,
+            value=self.value)
+        return output
+
+    class Meta:
+        verbose_name = _('用户元数据')
+        verbose_name_plural = verbose_name
