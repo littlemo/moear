1cbc73af9faab68d0c262f25423ea2c3152f2404
Moore.Huang <moore@moorehy.com>
2018-02-19 22:50:57 +0800

    F: 为posts组件中的数据模型创建后台管理类，并注册

--- server/posts/admin.py
+++ server/posts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import *
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'author', 'spider', 'origin_url',
+        'date', 'content', 'title', 'excerpt',
+        'status', 'modified')
+    search_fields = [
+        'author', 'date', 'content', 'title',
+        'excerpt']
+
+
+class PostMetaAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'post', 'name', 'value')
+    search_fields = [
+        'post__title', 'name', 'value']
+
+
+class ReadRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'post', 'user', 'star', 'comment')
+    search_fields = [
+        'post__title', 'user__username', 'star',
+        'comment']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostMeta, PostMetaAdmin)
+admin.site.register(ReadRecord, ReadRecordAdmin)
