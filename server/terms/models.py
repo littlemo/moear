918ea40f3f76994b129773717aecb70072a69f24
Moore.Huang <moore@moorehy.com>
2018-02-19 11:28:30 +0800

    F: 实现术语数据模型，用于实现分类系统

--- server/terms/models.py
+++ server/terms/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Term(models.Model):
+    """
+    术语数据模型
+
+    用于存储所有术语字段及其slug
+    """
+    id = models.BigAutoField(
+        primary_key=True)
+    name = models.CharField(
+        verbose_name=_('术语名'),
+        max_length=200)
+    slug = models.CharField(
+        verbose_name=_('Slug'),
+        unique=True,
+        max_length=200)
+
+    def __str__(self):
+        output = '[{id}]{name} => {slug}'.format(
+            id=self.id,
+            name=self.name,
+            slug=self.slug)
+        return output
+
+    class Meta:
+        verbose_name = _('术语数据')
+        verbose_name_plural = verbose_name
