0a4486515da2591b48e3812ae3bcf64373e7b96f
Moore.Huang <moore@moorehy.com>
2018-02-19 15:43:35 +0800

    F: 实现分类模型，用于扩展术语模型

--- server/terms/models.py
+++ server/terms/models.py
@@ -28,3 +28,35 @@ class Term(models.Model):
     class Meta:
         verbose_name = _('术语数据')
         verbose_name_plural = verbose_name
+
+
+class Taxonomy(models.Model):
+    """
+    分类模型
+
+    用于关联术语，为术语数据提供扩展属性定义
+    """
+    id = models.BigAutoField(
+        primary_key=True)
+    term = models.OneToOneField(
+        'Term',
+        verbose_name=_('术语'),
+        db_index=True,
+        on_delete=models.CASCADE)
+    taxonomy = models.CharField(
+        verbose_name=_('分类'),
+        unique=True,
+        max_length=32)
+    description = models.TextField(
+        blank=True,
+        default='',
+        verbose_name=_('描述'))
+    parent = models.ForeignKey(
+        'Taxonomy',
+        verbose_name=_('父分类'),
+        blank=True,
+        null=True,
+        default=None,
+        on_delete=models.SET_NULL)
+    count = models.BigIntegerField(
+        verbose_name=_('文章数统计'))
