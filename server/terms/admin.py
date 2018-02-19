46fa74fa813c4c7f9f1dc84465b904842c307454
Moore.Huang <moore@moorehy.com>
2018-02-19 23:06:53 +0800

    F: 为terms组件中的数据模型创建后台管理类，并注册

--- server/terms/admin.py
+++ server/terms/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import *
+
+
+class TermAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'slug')
+    search_fields = [
+        'name', 'slug']
+
+
+class TaxonomyAdmin(admin.ModelAdmin):
+    list_display = (
+        'term', 'taxonomy_type', 'description', 'parent',
+        'count')
+    search_fields = [
+        'term__name', 'description']
+    list_filter = ('taxonomy_type',)
+
+
+class relationshipsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'post', 'taxonomy', 'user')
+    search_fields = [
+        'post__title', 'taxonomy__term__name', 'user__username']
+
+
+admin.site.register(Term, TermAdmin)
+admin.site.register(Taxonomy, TaxonomyAdmin)
+admin.site.register(relationships, relationshipsAdmin)
