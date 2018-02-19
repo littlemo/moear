06b8f78f905cb048874bd1d0259b551ab918fbbe
Moore.Huang <moore@moorehy.com>
2018-02-19 22:59:35 +0800

    F: 为spdiers组件中的数据模型创建后台管理类，并注册

--- server/spiders/admin.py
+++ server/spiders/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import *
+
+
+class SpiderAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'display_name', 'author',
+        'email', 'description', 'enabled')
+    search_fields = [
+        'name', 'display_name', 'author',
+        'email', 'description']
+    list_filter = ('author',)
+
+
+class SpiderMetaAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'spider', 'name', 'value')
+    search_fields = [
+        'spider__name', 'name', 'value']
+
+
+admin.site.register(Spider, SpiderAdmin)
+admin.site.register(SpiderMeta, SpiderMetaAdmin)
