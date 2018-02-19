2ce2e696c22744a20c6388477425e9677cae5152
Moore.Huang <moore@moorehy.com>
2018-02-19 22:44:19 +0800

    F: 为核心组件中的数据模型创建后台管理类，并注册

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import *
+
+
+class OptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'value', 'autoload')
+    search_fields = [
+        'name', 'value']
+
+
+class UserMetaAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'name', 'value')
+    search_fields = [
+        'user__username', 'name', 'value']
+
+
+admin.site.register(Option, OptionAdmin)
+admin.site.register(UserMeta, UserMetaAdmin)
