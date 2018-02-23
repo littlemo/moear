9f4b8c80643825f8078b059d81712dc4d83ce389
Moore.Huang <moore@moorehy.com>
2018-02-23 11:56:14 +0800

    F: 实现Post序列化器create方法的定制修改，使其支持判断目标条目是否存在

--- server/posts/serializers.py
+++ server/posts/serializers.py
@@ -8,6 +8,15 @@ log = logging.getLogger(__name__)
 
 
 class PostSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        """若存在指定origin_url字段值的条目，则执行更新操作，否则执行创建"""
+        try:
+            post = Post.objects.get(
+                origin_url=validated_data.get('origin_url', ''))
+            return super().update(post, validated_data)
+        except Post.DoesNotExist:
+            return super().create(validated_data)
+
     class Meta:
         model = Post
         exclude = ('id', 'modified',)
