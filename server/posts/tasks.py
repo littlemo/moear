54fc34af536c3078b6302784a48899760b660e85
Moore.Huang <moore@moorehy.com>
2018-03-02 15:56:46 +0800

    F: 实现用于调用爬虫爬取Post的Celery任务

---
+++ server/posts/tasks.py
@@ -0,0 +1,38 @@
+import logging
+
+from django.utils import timezone
+from celery import shared_task
+from moear_spider_zhihudaily import entry
+
+from .serializers import *
+
+
+log = logging.getLogger(__name__)
+
+
+@shared_task
+def spider_post(spider_pk):
+    """
+    爬取文章数据
+    """
+    zhihu = entry.ZhihuDaily()
+    data = zhihu.crawl()
+
+    for d in data:
+        post_serializer = PostSerializer(data=d)
+        if not post_serializer.is_valid():
+            log.error(post_serializer.errors)
+            return
+        date = timezone.datetime.strptime(
+            d.get('date', ''), '%Y-%m-%d %H:%M:%S')
+        date = timezone.make_aware(date, timezone.get_current_timezone())
+        # TODO Fix 如果强制设置date(文章创建时间)
+        post_serializer.save()
+
+        metadata = d.pop('meta', [])
+        postmeta_serializer = PostMetaSerializer(
+            data=metadata, many=True)
+        if not postmeta_serializer.is_valid():
+            log.error(postmeta_serializer.errors)
+            return
+        postmeta_serializer.save(post=post_serializer.instance)
