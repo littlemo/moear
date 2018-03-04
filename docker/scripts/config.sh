db95dc8758b1647835e361cc18feaf43acfe3833
Moore.Huang <moore@moorehy.com>
2018-03-04 12:33:40 +0800

    F: 完善docker容器服务启动脚本，仅在调试模式下，执行对挂载Python包的重装

--- docker/scripts/config.sh
+++ docker/scripts/config.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 
+# 仅在调试模式下进行Python包的重新安装
+if [ "$DEBUG" = "True" ];then
+
+pip install --no-cache-dir -e /app/requirements/source/moear-spider-common
+pip install --no-cache-dir -e /app/requirements/source/moear-spider-*
+pip install --no-cache-dir -r /app/requirements/pip.txt
+
+fi
+
 # 初始化数据库表格
 python manage.py makemigrations --settings=$SERVER_SETTINGS --noinput
 python manage.py makemigrations --settings=$SERVER_SETTINGS --noinput posts spiders terms core
 python manage.py migrate --settings=$SERVER_SETTINGS --noinput
 
+# 仅在生产模式下执行静载资源归集&翻译文件生成等操作
 if [ "$PRODUCTION" = "True" ];then
 
 # 归集静态文件
