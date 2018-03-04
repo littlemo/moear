608a7fd62cb8b051907c74a56cd58b4abb869431
Moore.Huang <moore@moorehy.com>
2018-03-04 12:14:07 +0800

    F: 将Post爬取任务重构为基于stevedore的插件式实现

--- server/posts/tasks.py
+++ server/posts/tasks.py
@@ -3,7 +3,7 @@ import json
 
 from django.utils import timezone
 from celery import shared_task
-from moear_spider_zhihudaily import entry
+from stevedore import extension
 
 from .serializers import *
 
@@ -16,26 +16,38 @@ def spider_post(spider_pk):
     """
     爬取文章数据
     """
-    zhihu = entry.ZhihuDaily()
-    rc = zhihu.crawl()
-    log.info('爬取返回包：{}'.format(rc))
-    data = json.loads(rc, encoding='UTF-8')
+    mgr = extension.ExtensionManager(
+        namespace='moear.spider',
+        invoke_on_load=True,
+        invoke_args=(),
+    )
 
-    for d in data:
-        post_serializer = PostSerializer(data=d)
-        if not post_serializer.is_valid():
-            log.error(post_serializer.errors)
-            return
-        date = timezone.datetime.strptime(
-            d.get('date', ''), '%Y-%m-%d %H:%M:%S')
-        date = timezone.make_aware(date, timezone.get_current_timezone())
-        # TODO Fix 如果强制设置date(文章创建时间)
-        post_serializer.save()
+    def crawl(ext, *args):
+        rc = ext.obj.crawl()
+        log.info('[{name}]爬取返回包：{pack}'.format(
+            name=ext.name, pack=rc))
+        data = json.loads(rc, encoding='UTF-8')
+        return (ext.name, data)
 
-        metadata = d.pop('meta', [])
-        postmeta_serializer = PostMetaSerializer(
-            data=metadata, many=True)
-        if not postmeta_serializer.is_valid():
-            log.error(postmeta_serializer.errors)
-            return
-        postmeta_serializer.save(post=post_serializer.instance)
+    results = mgr.map(crawl)
+
+    for name, data in results:
+        log.info('[{name}]处理爬虫返回数据，并持久化'.format(name=name))
+        for d in data:
+            post_serializer = PostSerializer(data=d)
+            if not post_serializer.is_valid():
+                log.error(post_serializer.errors)
+                break
+            date = timezone.datetime.strptime(
+                d.get('date', ''), '%Y-%m-%d %H:%M:%S')
+            date = timezone.make_aware(date, timezone.get_current_timezone())
+            # TODO Fix 如果强制设置date(文章创建时间)
+            post_serializer.save()
+
+            metadata = d.pop('meta', [])
+            postmeta_serializer = PostMetaSerializer(
+                data=metadata, many=True)
+            if not postmeta_serializer.is_valid():
+                log.error(postmeta_serializer.errors)
+                break
+            postmeta_serializer.save(post=post_serializer.instance)
