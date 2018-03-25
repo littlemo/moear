e08e6f47e35031f3920743e4b4ac77ee75b9a366
Moore.Huang <moore@moorehy.com>
2018-03-25 22:37:35 +0800

    F: 实现SpiderMeta模型序列化器

--- server/spiders/serializers.py
+++ server/spiders/serializers.py
@@ -3,6 +3,7 @@ import copy
 
 from rest_framework import serializers
 from .models import *
+from core.serializers import MetaListSerializer
 
 
 log = logging.getLogger(__name__)
@@ -24,5 +25,27 @@ class SpiderSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'name': {
                 'validators': [],
+            },
+        }
+
+
+class SpiderMetaSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        '''若存在指定 Spider 的 name 字段数据，则执行更新操作，否则执行创建'''
+        try:
+            spidermeta = SpiderMeta.objects.get(
+                spider=validated_data.get('spider', ''),
+                name=validated_data.get('name', ''))
+            return super().update(spidermeta, validated_data)
+        except SpiderMeta.DoesNotExist:
+            return super().create(validated_data)
+
+    class Meta:
+        model = SpiderMeta
+        list_serializer_class = MetaListSerializer
+        exclude = ('id',)
+        extra_kwargs = {
+            'spider': {
+                'required': False,
             },
         }
