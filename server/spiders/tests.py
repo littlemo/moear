97a8914d10ad561360856b847b64a09d65df53c0
Moore.Huang <moore@moorehy.com>
2018-03-25 22:01:34 +0800

    F: 实现Spider序列化器的单元测试，并验证通过

--- server/spiders/tests.py
+++ server/spiders/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
 
-# Create your tests here.
+import logging
+
+from .serializers import *
+
+
+log = logging.getLogger(__name__)
+
+
+class SpiderSerializerTests(TestCase):
+    def setUp(self):
+        self.fake_data_spider = {
+            'name': 'zhihu_daily',
+            'display_name': '知乎日报',
+            'author': '小貘',
+            'email': 'moore@moorehy.com',
+            'description':
+                '每天三次，每次七分钟。在中国，资讯类移动应用的人均阅读时长是 '
+                '5 分钟，而在知乎日报，这个数字是 21',
+            'meta': {
+                'img_cover': 'img_cover_path',
+                'image_filter': ['equation\\?tex='],
+                'book_mode': 'periodical',
+                'language': 'zh-CN',
+                'img_masthead': 'img_masthead_path',
+                'css_package': 'css_package_path',
+                'package_module': 'mobi',
+            }
+        }
+
+    def test_create(self):
+        '''
+        根据砖数据进行Spider模型的序列化创建
+        '''
+        spider_serializer = SpiderSerializer(data=self.fake_data_spider)
+        if not spider_serializer.is_valid():
+            log.error(spider_serializer.errors)
+        self.assertTrue(spider_serializer.is_valid())
+
+        spider_serializer.save()
+
+        self.assertEqual(
+            Spider.objects.get(name=self.fake_data_spider.get('name')),
+            spider_serializer.instance)
