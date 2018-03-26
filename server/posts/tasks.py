9c85517836203acca6c31daa01961303c28fa33d
Moore.Huang <moore@moorehy.com>
2018-03-26 09:41:56 +0800

    F: 实现文章打包任务中的按spider分组逻辑

---
+++ server/posts/tasks.py
@@ -0,0 +1,27 @@
+import logging
+from collections import defaultdict
+
+from celery import shared_task
+from posts.models import *
+from posts.serializers import *
+from spiders.models import *
+from spiders.serializers import *
+
+log = logging.getLogger(__name__)
+
+
+@shared_task
+def package_post(post_pk_list):
+    '''
+    将传入的 Post 列表打包成 Mobi 文件
+    '''
+    post_list = [Post.objects.get(pk=pk) for pk in post_pk_list]
+    package_group = defaultdict(list)
+    for post in post_list:
+        postmeta_list = PostMeta.objects.filter(post=post)
+        postmeta_data = PostMetaSerializer(postmeta_list, many=True).data
+        post_data = PostSerializer(post).data
+        post_data['meta'] = postmeta_data
+        package_group[post.spider.name].append(post_data)
+
+    log.debug('根据Spider分组并序列化后的文章列表: {}'.format(package_group))
