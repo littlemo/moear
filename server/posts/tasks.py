1dee47e7e66d8b9b473ff6647e985a016fd62923
Moore.Huang <moore@moorehy.com>
2018-03-26 15:20:10 +0800

    F: 重构Post打包的分组字典生成逻辑，分别按打包方式、爬虫名分级

--- server/posts/tasks.py
+++ server/posts/tasks.py
@@ -20,12 +20,26 @@ def package_post(post_pk_list):
     # 将文章列表以时间倒序排列
     post_list.sort(key=lambda post: post.date, reverse=True)
 
-    package_group = defaultdict(list)
+    package_group = defaultdict(dict)
     for post in post_list:
         postmeta_list = PostMeta.objects.filter(post=post)
         postmeta_data = PostMetaSerializer(postmeta_list, many=True).data
         post_data = PostSerializer(post).data
         post_data['meta'] = postmeta_data
-        package_group[post.spider.name].append(post_data)
 
-    log.debug('根据Spider分组并序列化后的文章列表: {}'.format(package_group))
+        spider_name = post.spider.name
+
+        spider_data = SpiderSerializer(post.spider).data
+        spdiermeta_list = SpiderMeta.objects.filter(spider=post.spider)
+        spidermeta_data = SpiderMetaSerializer(spdiermeta_list, many=True).data
+        spider_data['meta'] = spidermeta_data
+
+        package_module = spidermeta_data.get('package_module', '')
+
+        package_group[package_module].setdefault(spider_name, {})
+        log.debug(package_group)
+        package_group[package_module][spider_name].setdefault(
+            'spider', spider_data)
+        log.debug(package_group)
+        package_group[package_module][spider_name].setdefault('data', [])
+        package_group[package_module][spider_name]['data'].append(post_data)
