c1ed005d455a808106e3b7311df46cadb71c78fd
Moore.Huang <moore@moorehy.com>
2018-03-26 11:53:17 +0800

    O: 为打包Post任务中的文章列表增加按发布时间倒序的排序逻辑

--- server/posts/tasks.py
+++ server/posts/tasks.py
@@ -16,6 +16,10 @@ def package_post(post_pk_list):
     将传入的 Post 列表打包成 Mobi 文件
     '''
     post_list = [Post.objects.get(pk=pk) for pk in post_pk_list]
+
+    # 将文章列表以时间倒序排列
+    post_list.sort(key=lambda post: post.date, reverse=True)
+
     package_group = defaultdict(list)
     for post in post_list:
         postmeta_list = PostMeta.objects.filter(post=post)
