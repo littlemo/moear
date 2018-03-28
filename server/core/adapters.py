7838b6caf6f153f25d8d90f3c26368a00126268b
Moore.Huang <moore@moorehy.com>
2018-03-28 14:41:43 +0800

    F: 实现 allauth 的账户适配器，定制邮件发送方法，使其支持异步发送

---
+++ server/core/adapters.py
@@ -0,0 +1,37 @@
+import logging
+
+from django.template import TemplateDoesNotExist
+from django.template.loader import render_to_string
+
+from allauth.account.adapter import DefaultAccountAdapter
+
+from .tasks import account_send_email_task
+
+log = logging.getLogger(__name__)
+
+
+class AccountAdapter(DefaultAccountAdapter):
+    '''
+    定制的 AllAuth 账户适配器，主要用于支持异步邮件发送
+    '''
+    def send_mail(self, template_prefix, email, context):
+        subject = render_to_string('{0}_subject.txt'.format(template_prefix),
+                                   context)
+        # remove superfluous line breaks
+        subject = " ".join(subject.splitlines()).strip()
+        subject = self.format_email_subject(subject)
+
+        from_email = self.get_from_email()
+
+        bodies = {}
+        for ext in ['html', 'txt']:
+            try:
+                template_name = '{0}_message.{1}'.format(template_prefix, ext)
+                bodies[ext] = render_to_string(template_name,
+                                               context).strip()
+            except TemplateDoesNotExist:
+                if ext == 'txt' and not bodies:
+                    # We need at least one body
+                    raise
+
+        account_send_email_task.delay(subject, bodies, from_email, email)
