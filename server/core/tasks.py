69f431a548312e41635bb0ca48f93f2ddb8e054b
Moore.Huang <moore@moorehy.com>
2018-03-28 14:41:08 +0800

    F: 实现 allauth 使用的异步邮件发送任务

---
+++ server/core/tasks.py
@@ -0,0 +1,27 @@
+import logging
+
+from celery import task
+from django.conf import settings
+from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.utils.translation import gettext_lazy as _
+
+log = logging.getLogger(__name__)
+
+
+@task(time_limit=settings.EMAIL_TIME_LIMIT)
+def account_send_email_task(subject, bodies, from_email, email):
+    if 'txt' in bodies:
+        msg = EmailMultiAlternatives(subject,
+                                     bodies['txt'],
+                                     from_email,
+                                     [email])
+        if 'html' in bodies:
+            msg.attach_alternative(bodies['html'], 'text/html')
+    else:
+        msg = EmailMessage(subject,
+                           bodies['html'],
+                           from_email,
+                           [email])
+        msg.content_subtype = 'html'  # Main content is now text/html
+    msg.send()
+    log.info(_('发送账户邮件到: %s'), email)
