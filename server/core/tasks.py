5adf87dfe14e69286d2e189ed9c0ceb0b519b435
Moore.Huang <moore@moorehy.com>
2018-03-29 11:56:49 +0800

    O: 优化账户相关邮件发送任务中的代码格式

--- server/core/tasks.py
+++ server/core/tasks.py
@@ -11,17 +11,19 @@ log = logging.getLogger(__name__)
 @task(time_limit=settings.EMAIL_TIME_LIMIT)
 def account_send_email_task(subject, bodies, from_email, email):
     if 'txt' in bodies:
-        msg = EmailMultiAlternatives(subject,
-                                     bodies['txt'],
-                                     from_email,
-                                     [email])
+        msg = EmailMultiAlternatives(
+            subject,
+            bodies['txt'],
+            from_email,
+            [email])
         if 'html' in bodies:
             msg.attach_alternative(bodies['html'], 'text/html')
     else:
-        msg = EmailMessage(subject,
-                           bodies['html'],
-                           from_email,
-                           [email])
+        msg = EmailMessage(
+            subject,
+            bodies['html'],
+            from_email,
+            [email])
         msg.content_subtype = 'html'  # Main content is now text/html
     msg.send()
     log.info(_('发送账户邮件到: %s'), email)
