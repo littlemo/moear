f4ed521fdd3e5739d6f8e87207fde0183fb84cf6
Moore.Huang <moore@moorehy.com>
2018-03-31 21:09:08 +0800

    F: 实现投递日志数据模型的后台管理类

--- server/deliver/admin.py
+++ server/deliver/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DeliverLog
+
+
+class DeliverLogAdmin(admin.ModelAdmin):
+    list_display = (
+        'date', 'spider', 'file_name', 'file_size', 'status')
+    search_fields = [
+        'file_name']
+    list_filter = ('date',)
+    date_hierarchy = 'date'
+
+
+admin.site.register(DeliverLog, DeliverLogAdmin)
