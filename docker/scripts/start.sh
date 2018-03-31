dc52048b114b79aa8d2281f7f3872c9fff1aa354
Moore.Huang <moore@moorehy.com>
2018-03-31 15:08:43 +0800

    F: 更新Docker启动脚本，重构为三个独立的Worker，并设置相应的并发配置

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -43,8 +43,14 @@ echo "Exec: celery beat"
 celery -A server beat -l $CELERY_BEAT_LOG_LEVEL --logfile=$CELERY_BEAT_LOG_FILE --pidfile= --detach
 
 # 启动celery worker服务
-echo "Exec: celery worker"
-celery -A server worker -l $CELERY_WORKER_LOG_LEVEL --logfile=$CELERY_WORKER_LOG_FILE --pidfile= --detach
+echo "Exec: celery worker [default]"
+celery -A server worker -Q default --hostname=default@%h -c $CELERY_WORKER_CONCURRENCY -l $CELERY_WORKER_LOG_LEVEL --logfile=$CELERY_WORKER_LOG_FILE --pidfile= --detach
+
+echo "Exec: celery worker [email]"
+celery -A server worker -Q email --hostname=email@%h -c $CELERY_WORKER_CONCURRENCY_EMAIL -l $CELERY_WORKER_LOG_LEVEL --logfile=$CELERY_WORKER_LOG_FILE --pidfile= --detach
+
+echo "Exec: celery worker [crawl]"
+celery -A server worker -Q crawl --hostname=crawl@%h -c $CELERY_WORKER_CONCURRENCY_CRAWL -l $CELERY_WORKER_LOG_LEVEL --logfile=$CELERY_WORKER_LOG_FILE --pidfile= --detach
 
 # 完成全部安装初始化工作后创建安装锁文件
 if [ ! -f "$INSTALL_LOCK_FILE" ]; then
