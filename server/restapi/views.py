af0beaccd4000165d51b4224396a3332fd3b8d52
Moore.Huang <moore@moorehy.com>
2018-04-05 12:32:27 +0800

    F: 为SpiderAPIView增加权限管理，仅管理员可访问

--- server/restapi/views.py
+++ server/restapi/views.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import permissions
 
 from spiders.models import Spider
 from spiders.serializers import SpiderSerializer
@@ -19,6 +20,8 @@ class SpiderAPIView(APIView):
 
     列出所有 Spider ，或者更新某一个 Spider 的开关状态
     '''
+    permission_classes = (permissions.IsAdminUser,)
+
     def get(self, request, format=None):
         spiders_obj = Spider.objects.all()
         spiders_list = SpiderSerializer(spiders_obj, many=True)
