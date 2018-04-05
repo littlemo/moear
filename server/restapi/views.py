988c960b8de81edc4514b1298fca7fca099728e4
Moore.Huang <moore@moorehy.com>
2018-04-05 12:44:27 +0800

    F: 实现用于更新爬虫开关状态的POST接口

--- server/restapi/views.py
+++ server/restapi/views.py
@@ -27,3 +27,26 @@ class SpiderAPIView(APIView):
         spiders_list = SpiderSerializer(spiders_obj, many=True)
         log.debug(_('爬虫列表: {}'.format(spiders_list.data)))
         return Response(spiders_list.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        '''
+        用例::
+
+            {
+                "name": "zhihu_daily",
+                "enabled": false
+            }
+        '''
+        spider_name = request.data.get('name', None)
+        spider_enabled = request.data.get('enabled', True)
+        if not spider_name:
+            return Response(_('name 字段为空'), status=status.HTTP_400_BAD_REQUEST)
+        try:
+            spider = Spider.objects.get(name=spider_name)
+        except Spider.DoesNotExist:
+            return Response(_('name({name}) 指定的 Spider 不存在').format(
+                name=spider_name), status=status.HTTP_404_NOT_FOUND)
+        spider.enabled = spider_enabled
+        spider.save()
+        serializer = SpiderSerializer(spider)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
