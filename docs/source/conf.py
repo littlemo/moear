003a43c6608711db0618c1063622e3d09c30c675
Moore.Huang <moore@moorehy.com>
2018-04-11 22:42:17 +0800

    M: 整理docs配置文件中的代码格式

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -208,7 +208,7 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3.5', None)
+    'python': ('https://docs.python.org/3.5', None),
 }
 
 # -- Options for todo extension ----------------------------------------------
