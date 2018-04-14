d5d0e019782f61afef33b8a683816da98901fea5
Moore.Huang <moore@moorehy.com>
2018-04-14 15:43:11 +0800

    F: 实现用于后台触发爬取打包投递的周期计划任务的action

--- server/spiders/admin.py
+++ server/spiders/admin.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from .models import *
 from . import tasks
 from celery import group
+from core.tasks import periodic_chain_crawl_package_deliver
 
 
 class SpiderAdmin(admin.ModelAdmin):
@@ -15,7 +16,9 @@ class SpiderAdmin(admin.ModelAdmin):
         'email', 'description']
     list_filter = ('author',)
     actions = [
-        'action_spider_post']
+        'action_spider_post',
+        'action_periodic_chain_crawl_package_deliver',
+    ]
 
     def action_spider_post(self, request, queryset):
         """异步爬取Post信息"""
@@ -28,7 +31,18 @@ class SpiderAdmin(admin.ModelAdmin):
             _('共触发 {num} 个爬虫源').format(
                 num=len(queryset)))
 
+    def action_periodic_chain_crawl_package_deliver(self, request, queryset):
+        '''异步周期链式爬取、打包并投递书籍'''
+        c = (group(
+            periodic_chain_crawl_package_deliver.s(
+                spider.name) for spider in queryset
+        ))
+        c.delay()
+        self.message_user(request, _('成功创建调用任务'))
+
     action_spider_post.short_description = _('异步爬取Post信息')
+    action_periodic_chain_crawl_package_deliver.short_description = \
+        _('异步周期链式爬取、打包并投递书籍')
 
 
 class SpiderMetaAdmin(admin.ModelAdmin):
