acb3f6d216d6e97d6b77d70d863313a40f0691cd
Moore.Huang <moore@moorehy.com>
2018-04-15 15:31:05 +0800

    F: 重构version模板变量的上下文处理器，从生成的version.py文件中获取版本号

--- server/core/context_processors.py
+++ server/core/context_processors.py
@@ -1,8 +1,10 @@
+from server.config import version as ver
+
 
 def version(request):
     """
     返回用于渲染模板的版本号上下文变量 ``version``
     """
     return {
-        'version': 'v1.0.0',
+        'version': ver.version_name,
     }
