964d1a5f078cab14178ed0048e5c7b9b1f1f2ea6
Moore.Huang <moore@moorehy.com>
2018-04-15 18:20:16 +0800

    O: 优化version的上下文处理方法，若import失败，则返回版本号为Unknown

--- server/core/context_processors.py
+++ server/core/context_processors.py
@@ -1,10 +1,13 @@
-from server.config import version as ver
-
 
 def version(request):
     """
     返回用于渲染模板的版本号上下文变量 ``version``
     """
+    try:
+        from server.config import version as ver
+        ver_name = ver.version_name
+    except ImportError:
+        ver_name = 'Unknown'
     return {
-        'version': ver.version_name,
+        'version': ver_name,
     }
